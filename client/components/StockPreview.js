da7d521031fd345ed6029b1a9ed9e53298205613
HagenGaryP <hagengaryp@gmail.com>
2021-02-14 14:44:54 -0500

    adjusted the card for better text display

--- client/components/StockPreview.js
+++ client/components/StockPreview.js
@@ -9,10 +9,13 @@ function StockPreview({ stock }) {
       <p className="stock-card-name">
         {stock.company}
       </p>
-      <div className="stock-card-textarea">
+      <div className="stock-ticker-and-price">
           {stock.ticker} : ${stock.price}
+      </div><br/>
+        <h5>Company description:</h5><br/>
+      <div className="stock-card-textarea">
+        {stock.description}
       </div>
-      {stock.description}
     </div>
   );
 }
