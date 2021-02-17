1b434b631258c135ebcf7f209a55b413ac87e812
HagenGaryP <hagengaryp@gmail.com>
2021-02-17 15:51:01 -0500

    corrected the price updating, but still needs to account for the date, since it only gets previous closing price for specified day

--- server/api/stocks.js
+++ server/api/stocks.js
@@ -52,16 +52,44 @@ router.delete('/:id', async (req, res, next) => {
 
 // Internal API call to search for a stock externally
 router.get('/search/:searchTerm', async (req, res, next) => {
-  let [month, date, year]    = new Date().toLocaleDateString("en-US").split("/")
-    console.log('month = ', month, '  date = ', date, ' year = ', year);
-    if (month < 10) {
-      month = '0' + month;
-    }
-    if (date < 10) {
-      date = '0' + date;
-    }
-    const today = year + '-' + month + '-' + date;
-    console.log('todays date = ', today);
+  let [month, date, year] = new Date().toLocaleDateString("en-US").split("/")
+  // making sure we get the last day the stock was traded, by setting date
+  switch (new Date().getDay()) {
+    case 1:
+      // day = "Monday";
+      if (date > 3) {
+        date -= 3;
+      }
+      // needs the else condition - this will include changing to previous month
+      // and also checking how many days are in the previous month
+      break;
+    case 0:
+      // sunday
+      if (date > 2) {
+        date -= 2;
+      }
+      break;
+    case 6:
+      // saturday
+      if (date > 1) {
+        date -= 1;
+      }
+      break;
+    default:
+      if (date > 1) {
+        date -= 1;
+      }
+      break;
+  }
+  console.log('month = ', month, '  date = ', date, ' year = ', year);
+  if (month < 10) {
+    month = '0' + month;
+  }
+  if (date < 10) {
+    date = '0' + date;
+  }
+  const previousDate = year + '-' + month + '-' + date;
+  // console.log('previous date = ', previousDate);
   try {
     const stockTicker = req.params.searchTerm.toUpperCase();
 
@@ -69,34 +97,35 @@ router.get('/search/:searchTerm', async (req, res, next) => {
     const { data } = await axios.get(`https://api.polygon.io/v2/reference/tickers?sort=ticker&locale=us&search=${stockTicker}&perpage=5&page=1&apiKey=${API_KEY}`)
     // response = await fetchStock(req.params.searchTerm);
     // this responds with the price info for that stock
-    const response = await axios.get(`https://api.polygon.io/v2/aggs/ticker/${stockTicker}/range/1/day/${today}/${today}?apiKey=${API_KEY}`);
+    const response = await axios.get(`https://api.polygon.io/v2/aggs/ticker/${stockTicker}/range/1/day/${previousDate}/${previousDate}?apiKey=${API_KEY}`);
 
     console.log('data >>>>> ', data);
     console.log('response.data >>>>> ', response.data);
 
-    // add the stock to DB
-    if (data.tickers[0]) {
-      try {
-        // setting current price key/value on data obj
-        if (response.data.results) {
-          console.log('RESPONSE DATA RESULTS =======', response.data.results);
-          // data.price = response.data.results[0].c;
-        }
-        console.log('data.price = ', data.price)
-        const stock = await Stock.create({
-          ticker: data.tickers[0].ticker,
-          company: data.tickers[0].name,
-          price: data.price
-        })
-        await stock.save();
-      } catch (error) {
-        console.log('error = ', error);
-      }
+    // add the stock to DB and update the price
+    // setting current price key/value on data obj
+    if (response.data.results) {
+      console.log('RESPONSE DATA RESULTS =======', response.data.results);
+      data.price = response.data.results[0].c;
     }
-    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ');
-    console.log(' DATA =  ', data);
-    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ');
-    res.send(data)
+    console.log('data.price = ', data.price)
+    const stockToUpdate = await Stock.findOrCreate({
+      where: {
+        ticker: data.tickers[0].ticker,
+        company: data.tickers[0].name,
+      }
+    })
+    const [rowsUpdated, updatedStock] = await Stock.update(
+      {
+        price: data.price,
+      },
+      {
+        where: {ticker: stockTicker},
+        returning: true,
+        plain: true,
+      }
+    )
+    res.json(updatedStock);
   } catch (error) {
     next(error);
   }
